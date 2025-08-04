245e778f19d80d27d336051d229f87f5ecfb9e57
Selim Yay <selimyay@192.168.1.35>
2025-08-05 01:25:51 +0300

    bug-fix-2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,20 +10,27 @@ export default function Home() {
   function handleClick() {
     setLoading(true);
     setTimeout(() => {
-      const task = tasks[Math.floor(Math.random() * tasks.length)].task
-      setTask(task)
-      setLoading(false)
-    }, 5000)
+      const task = tasks[Math.floor(Math.random() * tasks.length)].task;
+      setTask(task);
+      setLoading(false);
+    }, 5000);
   }
 
   return (
     <div className="h-screen flex flex-col items-start mt-12">
-      <h1 className="text-7xl font-semibold text-gradient animate-slide-in-left">Break the cycle, now. Feel Alive.</h1>
+      <h1 className="text-7xl font-semibold text-gradient animate-slide-in-left">
+        Break the cycle, now. Feel Alive.
+      </h1>
+
       {!loading && !task && (
-        <button className="rounded-full w-[50%] max-md:w-full mx-auto p-4 mt-18 hover-gradient" onClick={handleClick}>
-          Take Action, tell your brain that you are alive.
+        <button
+          className="rounded-full w-[50%] max-md:w-full mx-auto p-4 mt-18 hover-gradient"
+          onClick={handleClick}
+        >
+          Take Action, tell your brain that you&apos;re alive.
         </button>
       )}
+
       {loading && (
         <div className="mt-18 flex justify-center items-center w-full">
           <ClipLoader color="#8e44ad" size={50} />
@@ -32,10 +39,9 @@ export default function Home() {
 
       {task && (
         <div className="mt-18 flex justify-center items-center w-[50%] mx-auto max-md:w-full text-xl italic p-4 bg-black border-2 border-white rounded-full text-center">
-          "{task}"
+          &quot;{task}&quot;
         </div>
       )}
-
     </div>
   );
 }
