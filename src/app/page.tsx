32ebfb187d9a0c0fdc1a2a613dd9dedcf927f552
Selim Yay <selimyay@192.168.1.35>
2025-08-08 18:09:00 +0300

    home-ui-improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-"use client";
-import { useState } from "react";
-import { tasks } from "./data/tasks";
-import { ClipLoader } from "react-spinners";
+"use client"
+import Image from "next/image"
+import { useState } from "react"
+import { tasks } from "./data/tasks"
+import { ClipLoader } from "react-spinners"
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
@@ -23,26 +24,51 @@ export default function Home() {
         Break the cycle, now. Feel Alive.
       </h1>
 
-      {!loading && !task && (
-        <button
-          className="rounded-full w-[50%] max-md:w-full mx-auto p-4 mt-18 hover-gradient"
-          onClick={handleClick}
-        >
-          Take Action, tell your brain that you are alive.
-        </button>
-      )}
+      <div className="relative w-full h-[50%] mt-8 rounded-lg overflow-hidden">
+        <Image
+          src="/blue.jpg"
+          alt="blue"
+          fill
+          className="object-cover rounded-xl opacity-30"
+        />
 
-      {loading && (
-        <div className="mt-18 flex justify-center items-center w-full">
-          <ClipLoader color="#8e44ad" size={50} />
-        </div>
-      )}
+        <div className="absolute inset-0 flex flex-col items-center justify-center w-[70%] max-md:w-full mx-auto">
 
-      {task && (
-        <div className="mt-18 flex justify-center items-center w-[50%] mx-auto max-md:w-full text-xl italic p-4 backdrop-blur-3xl border-2 border-white rounded-full text-center">
-          &quot;{task}&quot;
+          <div className="flex max-md:flex-col items-center justify-evenly max-md:gap-8">
+            <h1 className="text-white text-2xl  text-center px-4 underline">
+              One step forward is greater than no step at all.
+            </h1>
+            <div className="w-px font-bold h-25 bg-white mx-8 max-md:hidden"></div>
+            <h1 className="text-white text-2xl text-center px-4 underline">
+              Take action, tell your brain that you are alive.
+            </h1>
+          </div>
+
+          {!loading && !task && (
+            <button
+              className="rounded-full w-[50%] max-md:w-full mx-auto p-4 mt-18 hover-gradient"
+              onClick={handleClick}
+            >
+              Start
+            </button>
+          )}
+          {loading && (
+            <div className="mt-18 flex justify-center items-center w-full">
+              <ClipLoader color="#8e44ad" size={50} />
+            </div>
+          )}
+
+          {task && (
+            <div className="mt-18 flex justify-center items-center w-[50%] mx-auto max-md:w-full text-xl italic p-4 backdrop-blur-3xl border-2 border-white rounded-full text-center">
+              &quot;{task}&quot;
+            </div>
+          )}
         </div>
-      )}
+      </div>
+
+
+
+
     </div>
   );
 }
